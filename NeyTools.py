3031fda5b9ce41e401efdcdde21407b1377713cb
SketheR <harangi.lajos.1996@gmail.com>
2019-08-19 20:57:41 +0200

    Added automatic hiding of options that are not available

--- NeyTools.py
+++ NeyTools.py
@@ -6,12 +6,16 @@ from shutil import which
 #Used to enable logging and development tools
 NT_DEVMODE = True
 
-#Used for disabling the bash option when bash is not available
+#Used for disabling options when they are not available
 NT_BASHAVAILABLE = False
+NT_CMDAVAILABLE = False
+NT_PSAVAILABLE = False
  
 def plugin_loaded():
-	global NT_BASHAVAILABLE
+	global NT_BASHAVAILABLE, NT_CMDAVAILABLE, NT_PSAVAILABLE
 	NT_BASHAVAILABLE = which('bash') is not None
+	NT_CMDAVAILABLE = which('cmd') is not None
+	NT_PSAVAILABLE = which('powershell') is not None
 
 #Base Class
 class NTBase(sublime_plugin.TextCommand):
@@ -52,6 +56,9 @@ class NeyToolsDebugTriggerCommand(NTBase):
 	"""Used for triggering the base class, while in developement."""
 
 	def run(self, edit):
+		print(NT_BASHAVAILABLE)
+		print(NT_CMDAVAILABLE)
+		print(NT_PSAVAILABLE)
 		self.execute('exit')
 
 	def is_visible(self):
@@ -62,13 +69,22 @@ class NeyToolsDebugTriggerCommand(NTBase):
 
 
 #Windows Tools (Windows Command Prompt)
-class OpenCmdCommand(NTBase):
+class NTCMDBase(NTBase):
+	"""The base of all Windows Command Prompt commands."""
+
+	def is_visible(self):
+		return NT_CMDAVAILABLE
+
+	def is_enabled(self):
+		return NT_CMDAVAILABLE
+
+class OpenCmdCommand(NTCMDBase):
 	"""Opens a new Windows Command Prompt in the current directory."""
 
 	def run(self, edit):
 		self.executeFromHere('start cmd')
 
-class RunPythonWinCommand(NTBase):
+class RunPythonWinCommand(NTCMDBase):
 	"""Runs the current python document, using the currently installed windows version of python3."""
 
 	def run(self, edit):
@@ -76,26 +92,45 @@ class RunPythonWinCommand(NTBase):
 
 
 #Windows Tools (PowerShell)
-class OpenPowerShellCommand(NTBase):
+class NTPSBase(NTBase):
+	"""The base of all Windows PowerShell commands."""
+
+	def is_visible(self):
+		return NT_PSAVAILABLE
+
+	def is_enabled(self):
+		return NT_PSAVAILABLE
+
+class OpenPowerShellCommand(NTPSBase):
 	"""Opens a new PowerShell terminal in the current directory."""
 
 	def run(self, edit):
 		self.executeFromHere('start powershell')
 
-class RunPowerShellCommand(NTBase):
+class RunPowerShellCommand(NTPSBase):
 	"""Runs the current PowerShell document."""
+
 	def run(self, edit):
 		self.executeFromHere('start cmd /K "powershell ./{file} & pause & exit"')
 
 
 #Linux Tools (Bash - Windows Subsystem for Linux)
-class OpenBashCommand(NTBase):
+class NTBashBase(NTBase):
+	"""The base of all Bash commands."""
+
+	def is_visible(self):
+		return NT_BASHAVAILABLE
+
+	def is_enabled(self):
+		return NT_BASHAVAILABLE
+
+class OpenBashCommand(NTBashBase):
 	"""Opens a new Bash terminal in the current directory."""
 
 	def run(self, edit):
 		self.executeFromHere('start bash')
 
-class RunPythonBashCommand(NTBase):
+class RunPythonBashCommand(NTBashBase):
 	"""Runs the current python document, using the currently installed linux version of python3."""
 
 	def run(self, edit):
